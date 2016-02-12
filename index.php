77a31cc0ab3bcd252591162b04f896351cd4ea9c
dangele-mac <dangele@medjugorje.local>
2016-02-12 16:17:38 +0000

    Add first files of the project
    Contains main page, styles and images.

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<title>Christina Geoghegan</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link rel="stylesheet" href="style/index.css" type="text/css">
+		<link href='https://fonts.googleapis.com/css?family=Calligraffitti' rel='stylesheet' type='text/css'>
+
+	</head>
+	<body>
+	<header>
+		<nav class="navbar shadow">
+				  <p class="title"> Christina Geoghegan </p>
+				  <ul class= "menu">
+				    <li> 
+				    	<a href="/" class="underline">Home</a>
+				    </li>
+				    <li>
+				        <a href="/print" class="underline">Portfolio</a>
+				    </li>
+				    <li>
+				        <a href="/web" class="underline">Exhibition</a>
+				    </li>
+				    <li>
+				        <a href="/bio" class="underline">Biography</a>
+				    </li>
+				    <li>
+				        <a href="/contact" class="underline">Contact</a>
+				    </li>
+				  </ul>
+				
+		</nav>
+	</header>
+		<div class="wrapper">
+			<aside>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+			</aside>	
+			<section class="main">
+			</section>
+		</div>
+		<footer>
+			 	<a href="/bio"><img src="images/fb.png" class="icon element"></a>
+				<a href="/bio"><img src="images/linkedin.png" class="icon element"></a>
+				<p>Christina Geoghegan - 2016</p>
+		</footer>
+	</body>
+</html>
+
